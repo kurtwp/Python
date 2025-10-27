963dbf2b3853ac522c26d543f74570bd7e4523c1
Kurt Pasewaldt <kurt@pasewaldt.com>
2025-10-27 16:52:46 -0400

    Update createRates2.py

--- createRates2.py
+++ createRates2.py
@@ -3,7 +3,7 @@ import random
 with open("6-digit-dn.csv", "r") as file:
     for lines in file:
         randomNumber = random.randint(1, 10000)
-        line = lines.rstrip('\n')
+        line = lines.rstrip('\n') # Similar to chomp in perl
         formatted_rate = f"0.{randomNumber:04d}"  # ensures 4 digits with leading zeros
         with open("rates.txt", "a") as outfile:
             outfile.writelines(f"{line},{formatted_rate}\n")
